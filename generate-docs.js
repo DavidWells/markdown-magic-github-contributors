5f83bd9e3bbd7325ff0a8df54066bd5ab5f3266f
davidwells <david@inboundnow.com>
2017-01-15 19:06:03 -0800

    remove script

--- generate-docs.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const path = require('path')
-const markdownMagic = require('markdown-magic')
-
-const config = {}
-
-const markdownPath = path.join(__dirname, 'README.md')
-markdownMagic(markdownPath, config)
